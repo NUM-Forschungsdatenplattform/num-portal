46f6017af815dd66cbac380f0b02c07298281055
Daniela Tomoiaga <daniela.tomoiaga.ext@vitasystems.de>
2020-09-30 15:26:14 +0300

    NUM-108 Project skeleton

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# research-portal
+# Num-portal
+
+Spring boot shell project
+
+### Database
+
+H2 in memory database setup
+Admin console can be accessed at http://localhost:8080/h2/
---
+++ src/main/java/de/vitagroup/num/NumPortalApplication.java
@@ -0,0 +1,11 @@
+package de.vitagroup.num;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumPortalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NumPortalApplication.class, args);
+    }
+}
