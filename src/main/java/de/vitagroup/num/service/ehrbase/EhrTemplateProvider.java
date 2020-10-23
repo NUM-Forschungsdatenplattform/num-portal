9f825f6b9ef21aa347118bfb9e07488e24ab5139
Alexander Karle <alexander.karle@vitasystems.de>
2020-10-23 14:18:59 +0200

    feat(cohort execution): integration of ehrbase SDK
    - updated cahnged package path of ehrbase templateprovider
    
    relates to #NUM-267

--- src/main/java/de/vitagroup/num/service/ehrbase/EhrTemplateProvider.java
+++ src/main/java/de/vitagroup/num/service/ehrbase/EhrTemplateProvider.java
@@ -1,7 +1,7 @@
 package de.vitagroup.num.service.ehrbase;
 
 import java.util.Optional;
-import org.ehrbase.client.templateprovider.TemplateProvider;
+import org.ehrbase.webtemplate.templateprovider.TemplateProvider;
 import org.openehr.schemas.v1.OPERATIONALTEMPLATE;
 
 public class EhrTemplateProvider implements TemplateProvider {
