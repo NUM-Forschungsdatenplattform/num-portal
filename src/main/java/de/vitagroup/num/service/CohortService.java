ee64f256c313835d831e04535b093f232ee01b94
Alexander Karle <alexander.karle@vitasystems.de>
2020-10-26 16:05:54 +0100

    refactor(cohort execution): integration of ehrbase SDK
    - fixed formatting
    
    relates to #NUM-267

--- src/main/java/de/vitagroup/num/service/CohortService.java
+++ src/main/java/de/vitagroup/num/service/CohortService.java
@@ -4,7 +4,6 @@ import de.vitagroup.num.domain.Cohort;
 import de.vitagroup.num.domain.repository.CohortRepository;
 import de.vitagroup.num.service.ehrbase.EhrBaseService;
 import java.util.List;
-import java.util.UUID;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -27,15 +26,10 @@ public class CohortService {
   public List<String> executeCohort(long cohortId) {
     // TODO: execute cohort and remove mocked list
     // Optional<Cohort> cohort =  cohortRepository.findById(cohortId);
-    // ehrBaseService.getPatientIds("SELECT e/ehr_id/value FROM EHR e df");
-
-    return List.of(UUID.randomUUID().toString(), UUID.randomUUID().toString());
+    return ehrBaseService.getPatientIds("SELECT e/ehr_id/value FROM EHR e");
   }
 
   public long getCohortSize(long cohortId) {
-    // TODO: execute cohort and remove mocked list
-    // Optional<Cohort> cohort =  cohortRepository.findById(cohortId);
-    // ehrBaseService.getPatientIds("SELECT e/ehr_id/value FROM EHR e df");
     return executeCohort(cohortId).size();
   }
 }
