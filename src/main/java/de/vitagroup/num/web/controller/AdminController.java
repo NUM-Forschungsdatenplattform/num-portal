af17fb2e350e8952d234a392ea8b85e88457c4f2
joni <joni.hahkala@vitaliberty.de>
2020-10-28 19:56:14 +0100

    feat: add user
    - clean merge problem

--- src/main/java/de/vitagroup/num/web/controller/AdminController.java
+++ src/main/java/de/vitagroup/num/web/controller/AdminController.java
@@ -9,7 +9,6 @@ import java.util.Set;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
