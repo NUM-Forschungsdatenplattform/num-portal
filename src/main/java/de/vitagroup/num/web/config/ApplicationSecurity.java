666262f7d59b5bbeaa2a25a007c6e318e9fe1a78
joni <joni.hahkala@vitaliberty.de>
2020-11-03 13:02:59 +0100

    feat: turn on oauth for incoming calls
    relates to #NUM-124

--- src/main/java/de/vitagroup/num/web/config/ApplicationSecurity.java
+++ src/main/java/de/vitagroup/num/web/config/ApplicationSecurity.java
@@ -1,14 +1,27 @@
 package de.vitagroup.num.web.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
+@EnableWebSecurity
 public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
 
   @Override
-  public void configure(WebSecurity web) {
-    web.ignoring().antMatchers("/**");
+  public void configure(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity
+        .httpBasic()
+        .disable()
+        .formLogin(AbstractHttpConfigurer::disable)
+        .authorizeRequests(authorize -> authorize.anyRequest().authenticated())
+        .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+        .sessionManagement(
+            sessionManagement ->
+                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
   }
 }
