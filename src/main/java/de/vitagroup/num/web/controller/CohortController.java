b8323a5a7052990819bd2dd859ed8c3ed659461e
Alexander Karle <alexander.karle@vitasystems.de>
2020-11-05 17:41:58 +0100

    feat(cohort execution): execute stored cohort
    - integrated cohort execution
    
    relates to #NUM-105

--- src/main/java/de/vitagroup/num/web/controller/CohortController.java
+++ src/main/java/de/vitagroup/num/web/controller/CohortController.java
@@ -4,7 +4,6 @@ import de.vitagroup.num.converter.CohortConverter;
 import de.vitagroup.num.domain.Cohort;
 import de.vitagroup.num.domain.dto.CohortDto;
 import de.vitagroup.num.service.CohortService;
-import de.vitagroup.num.web.exception.BadRequestException;
 import io.swagger.annotations.ApiOperation;
 import java.util.List;
 import java.util.Set;
@@ -12,7 +11,6 @@ import java.util.stream.Collectors;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
-import org.ehrbase.client.exception.WrongStatusCodeException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -48,21 +46,12 @@ public class CohortController {
   @PostMapping("{cohortId}/execute")
   @ApiOperation(value = "Executes the cohort")
   public ResponseEntity<Set<String>> executeCohort(@PathVariable String cohortId) {
-    try {
-      Set<String> patientIds = cohortService.executeCohort(Long.parseLong(cohortId));
-      return ResponseEntity.ok(patientIds);
-    } catch (WrongStatusCodeException | BadRequestException e) {
-      return ResponseEntity.badRequest().build();
-    }
+    return ResponseEntity.ok(cohortService.executeCohort(Long.parseLong(cohortId)));
   }
 
   @PostMapping("{cohortId}/size")
   @ApiOperation(value = "Retrieves the cohort size")
   public ResponseEntity<Long> retrieveCohortSize(@PathVariable String cohortId) {
-    try {
-      return ResponseEntity.ok(cohortService.getCohortSize(Long.parseLong(cohortId)));
-    } catch (WrongStatusCodeException e) {
-      return ResponseEntity.badRequest().build();
-    }
+    return ResponseEntity.ok(cohortService.getCohortSize(Long.parseLong(cohortId)));
   }
 }
--- src/main/java/de/vitagroup/num/web/exception/RestExceptionHandler.java
+++ src/main/java/de/vitagroup/num/web/exception/RestExceptionHandler.java
@@ -1,6 +1,11 @@
 package de.vitagroup.num.web.exception;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
+import org.ehrbase.client.exception.WrongStatusCodeException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,43 +15,51 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Slf4j
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        List<String> errors = ex
-                .getBindingResult()
-                .getFieldErrors()
-                .stream()
-                .map(fieldError -> fieldError.getDefaultMessage())
-                .collect(Collectors.toList());
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(
+      MethodArgumentNotValidException ex,
+      HttpHeaders headers,
+      HttpStatus status,
+      WebRequest request) {
+    List<String> errors =
+        ex.getBindingResult().getFieldErrors().stream()
+            .map(DefaultMessageSourceResolvable::getDefaultMessage)
+            .collect(Collectors.toList());
 
-        ErrorResponse response = ErrorResponse.builder().errors(errors).build();
-        return new ResponseEntity<>(response, headers, HttpStatus.BAD_REQUEST);
-    }
+    ErrorResponse response = ErrorResponse.builder().errors(errors).build();
+    return new ResponseEntity<>(response, headers, HttpStatus.BAD_REQUEST);
+  }
 
+  @ExceptionHandler({Exception.class})
+  public ResponseEntity<ErrorResponse> handleAll(Exception ex) {
+    log.error(ex.getMessage(), ex);
 
-    @ExceptionHandler({Exception.class})
-    public ResponseEntity<ErrorResponse> handleAll(Exception ex) {
-        log.error(ex.getMessage(), ex);
+    ErrorResponse response =
+        ErrorResponse.builder()
+            .errors(Collections.singletonList("An unexpected error has occurred"))
+            .build();
+    return new ResponseEntity<>(response, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+  }
 
-        ErrorResponse response = ErrorResponse.builder().errors(Arrays.asList("An unexpected error has occurred")).build();
-        return new ResponseEntity<>(response, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+  @ExceptionHandler({BadRequestException.class})
+  public ResponseEntity<ErrorResponse> handleBadRequest(Exception ex) {
+    log.debug(ex.getMessage(), ex);
 
-    @ExceptionHandler({BadRequestException.class})
-    public ResponseEntity<ErrorResponse> handleBadRequest(Exception ex) {
-        log.debug(ex.getMessage(), ex);
+    ErrorResponse response =
+        ErrorResponse.builder().errors(Collections.singletonList(ex.getMessage())).build();
+    return new ResponseEntity<>(response, new HttpHeaders(), HttpStatus.BAD_REQUEST);
+  }
 
-        ErrorResponse response = ErrorResponse.builder().errors(Arrays.asList(ex.getMessage())).build();
-        return new ResponseEntity<>(response, new HttpHeaders(), HttpStatus.BAD_REQUEST);
-    }
+  @ExceptionHandler({WrongStatusCodeException.class})
+  public ResponseEntity<ErrorResponse> handleWrongStatus(WrongStatusCodeException ex) {
+    log.error(ex.getMessage(), ex);
 
+    ErrorResponse response =
+        ErrorResponse.builder().errors(Collections.singletonList(ex.getMessage())).build();
+    return new ResponseEntity<>(response, new HttpHeaders(), HttpStatus.BAD_REQUEST);
+  }
 }
-
