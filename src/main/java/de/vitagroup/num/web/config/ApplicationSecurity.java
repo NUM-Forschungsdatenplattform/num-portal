e671d609f6f2681908820770da45c9d82079810b
Alexander Karle <alexander.karle@vitasystems.de>
2020-12-01 14:50:56 +0100

    feat(NUM-593): add cors settings
    - added cors settings and config parameters

--- src/main/java/de/vitagroup/num/web/config/ApplicationSecurity.java
+++ src/main/java/de/vitagroup/num/web/config/ApplicationSecurity.java
@@ -1,5 +1,7 @@
 package de.vitagroup.num.web.config;
 
+import de.vitagroup.num.properties.CorsProperties;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -8,12 +10,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
 
   private static final String[] AUTH_WHITELIST = {"/swagger-*/**", "/v2/**", "/v3/**"};
+  private static final String BASE_PATH = "/**";
+
+  private final CorsProperties corsProperties;
 
   @Override
   public void configure(HttpSecurity httpSecurity) throws Exception {
@@ -25,11 +34,24 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
         .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
         .sessionManagement(
             sessionManagement ->
-                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .cors()
+        .configurationSource(corsConfigurationSource());
   }
 
   @Override
   public void configure(WebSecurity web) {
     web.ignoring().antMatchers(AUTH_WHITELIST);
+  }
+
+  private CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration corsConfiguration = new CorsConfiguration();
+    corsConfiguration.applyPermitDefaultValues();
+    corsConfiguration.setAllowedOrigins(corsProperties.getAllowedOrigins());
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration(BASE_PATH, corsConfiguration);
+
+    return source;
   }
 }
