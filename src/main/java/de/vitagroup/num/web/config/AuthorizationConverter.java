d77dd2a975d48efe88c29dfb7cff6da0b98e3700
Alexander Karle <alexander.karle@vitasystems.de>
2020-12-09 16:39:35 +0100

    feat(NUM-611): role based access
    - added role mapping and method level role checks

--- src/main/java/de/vitagroup/num/web/config/AuthorizationConverter.java
+++ src/main/java/de/vitagroup/num/web/config/AuthorizationConverter.java
@@ -1,8 +1,9 @@
 package de.vitagroup.num.web.config;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.stream.Collectors;
+import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.collections4.SetUtils;
 import org.springframework.core.convert.converter.Converter;
@@ -17,7 +18,8 @@ import org.springframework.stereotype.Component;
 public class AuthorizationConverter implements Converter<Jwt, AbstractAuthenticationToken> {
 
   private static final String ROLE_PREFIX = "ROLE_";
-  private static final String ROLES_CLAIM = "groups";
+  private static final String REALM_ACCESS = "realm_access";
+  private static final String ROLES_CLAIM = "roles";
 
   @Override
   public AbstractAuthenticationToken convert(Jwt jwt) {
@@ -26,12 +28,15 @@ public class AuthorizationConverter implements Converter<Jwt, AbstractAuthentica
   }
 
   private Collection<GrantedAuthority> extractAuthorities(Jwt jwt) {
-    final List<String> roles = jwt.getClaim(ROLES_CLAIM);
+    JSONObject realmAccess = jwt.getClaim(REALM_ACCESS);
+    if (realmAccess != null) {
+      final JSONArray roles = (JSONArray) realmAccess.get(ROLES_CLAIM);
 
-    if (CollectionUtils.isNotEmpty(roles)) {
-      return roles.stream()
-          .map(role -> new SimpleGrantedAuthority(ROLE_PREFIX + role))
-          .collect(Collectors.toSet());
+      if (CollectionUtils.isNotEmpty(roles)) {
+        return roles.stream()
+            .map(role -> new SimpleGrantedAuthority(ROLE_PREFIX + role))
+            .collect(Collectors.toSet());
+      }
     }
     return SetUtils.emptySet();
   }
