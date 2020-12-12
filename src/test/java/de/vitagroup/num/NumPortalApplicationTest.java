fd7b5e233ba4aecf4a8467b318c6edbc6228178c
Daniela Tomoiaga <daniela.tomoiaga@vitagroup.ag>
2020-12-12 22:12:58 +0200

    feat(NUM-587): Unit tests

---
+++ src/test/java/de/vitagroup/num/NumPortalApplicationTest.java
@@ -0,0 +1,10 @@
+package de.vitagroup.num;
+
+import org.junit.Test;
+
+public class NumPortalApplicationTest {
+  @Test
+  public void shouldStart() {
+    NumPortalApplication.main(new String[] {});
+  }
+}
