a787551972c2799ac80fb0ab57d63681fa8adccb
Daniela Tomoiaga <daniela.tomoiaga@vitagroup.ag>
2021-01-12 18:41:32 +0200

    feat(NUM-836): Aql use and purpose

--- src/main/resources/db/migration/V13__aql_edit.sql
+++ src/main/resources/db/migration/V13__aql_edit.sql
@@ -1,3 +1,3 @@
 ALTER TABLE aql DROP COLUMN description;
-ALTER TABLE aql ADD COLUMN use varchar(250);
-ALTER TABLE aql ADD COLUMN purpose varchar(250);
+ALTER TABLE aql ADD COLUMN use text;
+ALTER TABLE aql ADD COLUMN purpose text;
