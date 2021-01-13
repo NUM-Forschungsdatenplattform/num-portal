ada0720c94b1858516e792b58ab7d841fa34330a
jhahkala <joni.hahkala@iki.fi>
2021-01-13 10:31:52 +0100

    feat(NUM-961): sync super admin string

--- src/main/java/de/vitagroup/num/web/config/Role.java
+++ src/main/java/de/vitagroup/num/web/config/Role.java
@@ -2,14 +2,14 @@ package de.vitagroup.num.web.config;
 
 public class Role {
 
-  public static final String ADMIN = "hasRole('ADMIN')";
+  public static final String SUPER_ADMIN = "hasRole('SUPER_ADMIN')";
   public static final String ORGANIZATION_ADMIN = "hasRole('ORGANIZATION_ADMIN')";
   public static final String STUDY_COORDINATOR = "hasRole('STUDY_COORDINATOR')";
   public static final String RESEARCHER = "hasRole('RESEARCHER')";
   public static final String STUDY_COORDINATOR_OR_RESEARCHER =
       "hasAnyRole('STUDY_COORDINATOR', 'RESEARCHER')";
-  public static final String STUDY_COORDINATOR_OR_ADMIN =
-      "hasAnyRole('STUDY_COORDINATOR', 'ADMIN')";
+  public static final String STUDY_COORDINATOR_OR_SUPER_ADMIN =
+      "hasAnyRole('STUDY_COORDINATOR', 'SUPER_ADMIN')";
 
   private Role() {
   }
