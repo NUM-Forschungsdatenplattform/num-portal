5cc1063e40e77dabcbf012479c57272c9effcfdd
jhahkala <joni.hahkala@iki.fi>
2021-02-11 18:30:08 +0100

    feat(NUM-1093):force select ehr ids only
    - relates to #NUM-1032

--- src/test/java/de/vitagroup/num/service/EhrBaseServiceTest.java
+++ src/test/java/de/vitagroup/num/service/EhrBaseServiceTest.java
@@ -1,9 +1,22 @@
 package de.vitagroup.num.service;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import de.vitagroup.num.domain.Aql;
 import de.vitagroup.num.service.ehrbase.EhrBaseService;
-import de.vitagroup.num.web.exception.SystemException;
+import java.util.List;
+import org.assertj.core.util.Lists;
+import org.ehrbase.aql.parser.AqlParseException;
+import org.ehrbase.client.aql.field.AqlFieldImp;
+import org.ehrbase.client.aql.query.EntityQuery;
 import org.ehrbase.client.aql.query.Query;
+import org.ehrbase.client.aql.record.Record;
 import org.ehrbase.client.exception.WrongStatusCodeException;
 import org.ehrbase.client.openehrclient.defaultrestclient.DefaultRestClient;
 import org.ehrbase.response.ehrscape.TemplateMetaDataDto;
@@ -12,17 +25,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Answers;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-
-import java.util.List;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsNull.notNullValue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class EhrBaseServiceTest {
@@ -47,13 +53,31 @@ public class EhrBaseServiceTest {
     templatesResponseData.set(List.of(t1, t2));
 
     when(restClient.templateEndpoint().findAllTemplates()).thenReturn(templatesResponseData);
-    when(restClient.aqlEndpoint().execute(any(Query.class)))
-        .thenThrow(WrongStatusCodeException.class);
   }
 
   @Test(expected = WrongStatusCodeException.class)
   public void shouldHandleBadAqlQuery() {
-    ehr.retrieveEligiblePatientIds(Aql.builder().build());
+    when(restClient.aqlEndpoint().execute(any(Query.class)))
+        .thenThrow(WrongStatusCodeException.class);
+    ehr.retrieveEligiblePatientIds(Aql.builder().query("SELECT e/ehr_id/value FROM EHR e").build());
+  }
+
+  @Test(expected = AqlParseException.class)
+  public void shouldHandleMalformedAqlQuery() {
+    when(restClient.aqlEndpoint().execute(any(Query.class)))
+        .thenThrow(WrongStatusCodeException.class);
+    ehr.retrieveEligiblePatientIds(Aql.builder().query("SLECT e/ehr_id/value FROM EHR e").build());
+  }
+
+  @Test
+  public void shouldReplaceSelect() {
+    when(restClient.aqlEndpoint().execute(any(Query.class))).thenReturn(Lists.emptyList());
+    ehr.retrieveEligiblePatientIds(Aql.builder().query("SELECT e/ehr_id FROM EHR e").build());
+    ArgumentCaptor<EntityQuery<Record>> queryArgumentCaptor =
+        ArgumentCaptor.forClass(EntityQuery.class);
+    verify(restClient.aqlEndpoint(), times(1)).execute(queryArgumentCaptor.capture());
+    AqlFieldImp fieldImp = (AqlFieldImp) queryArgumentCaptor.getValue().fields()[0];
+    assertThat(fieldImp.getPath(), is("/ehr_id/value"));
   }
 
   @Test
