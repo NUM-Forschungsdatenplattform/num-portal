8478af3749fbe452ce1e00ff61bb997b2697aca3
Daniela Tomoiaga <daniela.tomoiaga@vitagroup.ag>
2021-02-21 23:13:44 +0200

    feat(NUM-1185): Composition flattening optimization

--- src/main/java/de/vitagroup/num/service/ehrbase/CompositionFlattener.java
+++ src/main/java/de/vitagroup/num/service/ehrbase/CompositionFlattener.java
@@ -5,19 +5,37 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nedap.archie.rm.composition.Composition;
 import de.vitagroup.num.web.exception.SystemException;
 import java.util.Map;
-import lombok.AllArgsConstructor;
+import java.util.Optional;
+import javax.annotation.PostConstruct;
+import javax.cache.Cache;
+import javax.cache.CacheManager;
+import javax.cache.Caching;
+import javax.cache.configuration.MutableConfiguration;
+import javax.cache.expiry.CreatedExpiryPolicy;
+import javax.cache.expiry.Duration;
+import javax.cache.spi.CachingProvider;
 import org.ehrbase.aqleditor.service.TestDataTemplateProvider;
 import org.ehrbase.serialisation.flatencoding.FlatFormat;
 import org.ehrbase.serialisation.flatencoding.FlatJasonProvider;
 import org.ehrbase.serialisation.flatencoding.FlatJson;
 import org.ehrbase.util.exception.SdkException;
+import org.ehrbase.webtemplate.model.WebTemplate;
+import org.ehrbase.webtemplate.templateprovider.CachedTemplateProvider;
+import org.openehr.schemas.v1.OPERATIONALTEMPLATE;
 import org.springframework.stereotype.Component;
 
 @Component
-@AllArgsConstructor
 public class CompositionFlattener {
 
-  private final ObjectMapper mapper;
+  private final ObjectMapper mapper = new ObjectMapper();
+
+  private CachedTemplateProvider cachedTemplateProvider;
+
+  private Cache<String, FlatJson> flatJsonCache;
+
+  private static final String FLAT_JSON_CACHE = "flatJsonCache";
+  private static final String OPERATIONAL_TEMPLATE_CACHE = "operationalTemplateCache";
+  private static final String WEB_TEMPLATE_CACHE = "webTemplateCache";
 
   public Map<String, String> flatten(Composition composition) {
 
@@ -25,15 +43,27 @@ public class CompositionFlattener {
 
     try {
       String templateId = composition.getArchetypeDetails().getTemplateId().getValue();
-      FlatJson flattener =
-          new FlatJasonProvider(new TestDataTemplateProvider())
-              .buildFlatJson(FlatFormat.SIM_SDT, templateId);
-      return mapper.readValue(flattener.marshal(composition), Map.class);
+      return mapper.readValue(getFlatJson(templateId).marshal(composition), Map.class);
     } catch (JsonProcessingException e) {
       throw new UnsupportedOperationException("Cannot parse results");
-    } catch (SdkException e){
+    } catch (SdkException e) {
       throw new SystemException(e.getMessage());
     }
+  }
+
+  private FlatJson getFlatJson(String templateId) {
+    Optional<FlatJson> cachedFlatJson = Optional.ofNullable(flatJsonCache.get(templateId));
+
+    if (cachedFlatJson.isEmpty()) {
+      FlatJson flatJson =
+          new FlatJasonProvider(cachedTemplateProvider)
+              .buildFlatJson(FlatFormat.SIM_SDT, templateId);
+
+      flatJsonCache.put(templateId, flatJson);
+      return flatJson;
+    }
+
+    return cachedFlatJson.get();
   }
 
   private void validateComposition(Composition composition) {
@@ -43,5 +73,40 @@ public class CompositionFlattener {
       throw new UnsupportedOperationException(
           "Cannot parse results, composition missing template id");
     }
+  }
+
+  @PostConstruct
+  private void initializeTemplateCache() {
+    CachingProvider provider = Caching.getCachingProvider();
+    CacheManager cacheManager = provider.getCacheManager();
+
+    MutableConfiguration<String, OPERATIONALTEMPLATE> templateCacheConfig =
+        new MutableConfiguration<String, OPERATIONALTEMPLATE>()
+            .setTypes(String.class, OPERATIONALTEMPLATE.class)
+            .setExpiryPolicyFactory(CreatedExpiryPolicy.factoryOf(Duration.ONE_DAY))
+            .setStoreByValue(false);
+
+    Cache<String, OPERATIONALTEMPLATE> templateCache =
+        cacheManager.createCache(OPERATIONAL_TEMPLATE_CACHE, templateCacheConfig);
+
+    MutableConfiguration<String, WebTemplate> introspectCacheConfig =
+        new MutableConfiguration<String, WebTemplate>()
+            .setTypes(String.class, WebTemplate.class)
+            .setExpiryPolicyFactory(CreatedExpiryPolicy.factoryOf(Duration.ONE_DAY))
+            .setStoreByValue(false);
+
+    Cache<String, WebTemplate> introspectCache =
+        cacheManager.createCache(WEB_TEMPLATE_CACHE, introspectCacheConfig);
+
+    cachedTemplateProvider =
+        new CachedTemplateProvider(new TestDataTemplateProvider(), templateCache, introspectCache);
+
+    MutableConfiguration<String, FlatJson> flatJsonCacheConfig =
+        new MutableConfiguration<String, FlatJson>()
+            .setTypes(String.class, FlatJson.class)
+            .setExpiryPolicyFactory(CreatedExpiryPolicy.factoryOf(Duration.ONE_DAY))
+            .setStoreByValue(false);
+
+    flatJsonCache = cacheManager.createCache(FLAT_JSON_CACHE, flatJsonCacheConfig);
   }
 }
