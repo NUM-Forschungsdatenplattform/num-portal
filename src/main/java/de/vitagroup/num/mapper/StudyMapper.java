019985111e17cfeb2ce5782f8e07805cfac61e27
Daniela Tomoiaga <daniela.tomoiaga@vitagroup.ag>
2021-02-22 14:30:39 +0200

    feat(NUM-1086): Add coordinator to study

--- src/main/java/de/vitagroup/num/mapper/StudyMapper.java
+++ src/main/java/de/vitagroup/num/mapper/StudyMapper.java
@@ -8,7 +8,6 @@ import javax.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.PropertyMap;
-import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.stereotype.Component;
 
 @Component
