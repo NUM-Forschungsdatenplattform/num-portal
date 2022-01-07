0e504de8ab2bcd22b274eda9c6a3151a3f26f0ed
Daniela Tomoiaga <daniela.tomoiaga@vitagroup.ag>
2022-01-07 10:58:03 +0200

    feat(NUM-1936): Removes runtime default values for AqlDtos translated fields (name, purpose and use) and adds a migration script

--- src/main/resources/db/migration/V03__aql_fields_defaults.sql
+++ src/main/resources/db/migration/V03__aql_fields_defaults.sql
@@ -1,16 +1,16 @@
-UPDATE num.aql
+UPDATE aql
 SET use_translated = use
 WHERE use_translated IS NULL
   AND use IS NOT NULL
   AND use IS DISTINCT FROM use_translated;
 
-UPDATE num.aql
+UPDATE aql
 SET name_translated = name
 WHERE name_translated IS NULL
   AND name IS NOT NULL
   AND name IS DISTINCT FROM name_translated;
 
-UPDATE num.aql
+UPDATE aql
 SET purpose_translated = purpose
 WHERE purpose_translated IS NULL
   AND purpose IS NOT NULL
