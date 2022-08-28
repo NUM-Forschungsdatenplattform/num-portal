aaeeca1cea15dd7955e04d02b126713fdb9fc3a2
Marin <marin@bend.md>
2022-08-28 16:48:46 +0300

    [NUM-1963] - Enhance error-handling in NUM Core. Test Sonar Qube run with coverage percentage

--- src/test/java/de/vitagroup/num/service/CommentServiceTest.java
+++ src/test/java/de/vitagroup/num/service/CommentServiceTest.java
@@ -6,31 +6,44 @@ import static de.vitagroup.num.domain.templates.ExceptionsTemplate.COMMENT_NOT_F
 import static de.vitagroup.num.domain.templates.ExceptionsTemplate.INVALID_COMMENTID_ID;
 import static de.vitagroup.num.domain.templates.ExceptionsTemplate.PROJECT_DOES_NOT_EXIST;
 import static de.vitagroup.num.domain.templates.ExceptionsTemplate.PROJECT_NOT_FOUND;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThrows;
 import static org.mockito.Mockito.when;
 
 import java.time.OffsetDateTime;
+import java.util.Optional;
 
-import de.vitagroup.num.service.exception.BadRequestException;
-import de.vitagroup.num.service.exception.ForbiddenException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import de.vitagroup.num.domain.Comment;
-import de.vitagroup.num.domain.admin.User;
 import de.vitagroup.num.domain.admin.UserDetails;
-import de.vitagroup.num.domain.dto.CommentDto;
+import de.vitagroup.num.domain.repository.CommentRepository;
+import de.vitagroup.num.domain.repository.ProjectRepository;
+import de.vitagroup.num.service.exception.BadRequestException;
+import de.vitagroup.num.service.exception.ForbiddenException;
 import de.vitagroup.num.service.exception.ResourceNotFound;
-import org.springframework.dao.EmptyResultDataAccessException;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CommentServiceTest {
 
   @Mock private UserDetailsService userDetailsService;
 
-  @Mock private CommentService commentService;
+  @InjectMocks
+  private CommentService commentService;
+
+  @Mock private ProjectService projectService;
+
+  @Mock private CommentRepository commentRepository;
+
+  @Mock
+  private ProjectRepository projectRepository;
 
   private Comment comment;
 
@@ -41,79 +54,88 @@ public class CommentServiceTest {
     comment.setCreateDate(OffsetDateTime.now());
     comment.setId(1L);
 
-    when(commentService.getComments(1L, "userId"))
-            .thenThrow(new ResourceNotFound(CommentService.class, PROJECT_DOES_NOT_EXIST));
+    UserDetails approvedCoordinator =
+            UserDetails.builder().userId("approvedCoordinatorId").approved(true).build();
 
-    when(commentService.createComment(comment, 1L, "userId"))
-            .thenThrow(new ResourceNotFound(CommentService.class, PROJECT_NOT_FOUND, String.format(PROJECT_NOT_FOUND, 1L)));
+    when(projectService.exists(1L))
+            .thenReturn(true);
 
-    when(commentService.updateComment(comment, 1L, "userId", 1L))
-            .thenThrow(new ResourceNotFound(CommentService.class, PROJECT_NOT_FOUND, String.format(PROJECT_NOT_FOUND, 1L)));
-
-    when(commentService.updateComment(comment, 2L, "userId", 1L))
-            .thenThrow(new ResourceNotFound(CommentService.class, COMMENT_NOT_FOUND, String.format(COMMENT_NOT_FOUND, 2L)));
-
-    when(commentService.updateComment(comment, 3L, "userId", 1L))
-            .thenThrow( new ForbiddenException(CommentService.class, COMMENT_EDIT_FOR_COMMENT_WITH_ID_IS_NOT_ALLOWED_COMMENT_HAS_DIFFERENT_AUTHOR,
-            String.format(COMMENT_EDIT_FOR_COMMENT_WITH_ID_IS_NOT_ALLOWED_COMMENT_HAS_DIFFERENT_AUTHOR, 3L)));
-
-    when(commentService.deleteComment(1L, 1L, "userId"))
-            .thenThrow(new ResourceNotFound(CommentService.class, PROJECT_DOES_NOT_EXIST));
-
-    when(commentService.deleteComment(2L, 1L, "userId"))
-            .thenThrow(new ResourceNotFound(CommentService.class, COMMENT_NOT_FOUND, String.format(COMMENT_NOT_FOUND, 2L)));
-
-    when(commentService.deleteComment(3L, 1L, "userId"))
-            .thenThrow(new ForbiddenException(CommentService.class, CANNOT_DELETE_COMMENT, String.format(CANNOT_DELETE_COMMENT, 3L)));
-
-    when(commentService.deleteComment(4L, 1L, "userId"))
-            .thenThrow(new BadRequestException(EmptyResultDataAccessException.class, INVALID_COMMENTID_ID, String.format("%s: %s", INVALID_COMMENTID_ID, 4L)));
+    when(commentRepository.findById(3L))
+            .thenReturn(
+                    Optional.of(
+                            Comment.builder()
+                                    .id(3L)
+                                    .author(approvedCoordinator)
+                                    .build()));
 
   }
 
   @Test(expected = ResourceNotFound.class)
   public void getCommentsForNonExistingProject() {
-   commentService.getComments(1L, "userId");
+    when(commentService.getComments(2L, "userId"))
+            .thenThrow(new ResourceNotFound(CommentService.class, PROJECT_DOES_NOT_EXIST));
+    ResourceNotFound exception =
+            assertThrows(
+                    ResourceNotFound.class,
+                    () -> commentService.getComments(2L, "userId"));
+    assertThat(exception.getMessage(), is(PROJECT_DOES_NOT_EXIST));
   }
 
   @Test(expected = ResourceNotFound.class)
   public void createCommentForNonExistingProject() {
+    when(commentService.createComment(comment, 1L, "userId"))
+            .thenThrow(new ResourceNotFound(CommentService.class, PROJECT_NOT_FOUND, String.format(PROJECT_NOT_FOUND, 1L)));
     commentService.createComment(comment, 1L, "userId");
   }
 
   @Test(expected = ResourceNotFound.class)
   public void updateCommentForNonExistingProject() {
+    when(commentService.updateComment(comment, 1L, "userId", 1L))
+            .thenThrow(new ResourceNotFound(CommentService.class, PROJECT_NOT_FOUND, String.format(PROJECT_NOT_FOUND, 1L)));
     commentService.updateComment(comment, 1L, "userId", 1L);
   }
 
   @Test(expected = ResourceNotFound.class)
   public void updateCommentForNonExistingCommentInProject() {
+    when(commentService.updateComment(comment, 2L, "userId", 1L))
+            .thenThrow(new ResourceNotFound(CommentService.class, COMMENT_NOT_FOUND, String.format(COMMENT_NOT_FOUND, 2L)));
     commentService.updateComment(comment, 2L, "userId", 1L);
   }
 
   @Test(expected = ForbiddenException.class)
   public void updateCommentBelongToAnotherAuthor() {
+    when(commentService.updateComment(comment, 3L, "userId", 1L))
+            .thenThrow( new ForbiddenException(CommentService.class, COMMENT_EDIT_FOR_COMMENT_WITH_ID_IS_NOT_ALLOWED_COMMENT_HAS_DIFFERENT_AUTHOR,
+                    String.format(COMMENT_EDIT_FOR_COMMENT_WITH_ID_IS_NOT_ALLOWED_COMMENT_HAS_DIFFERENT_AUTHOR, 3L)));
     commentService.updateComment(comment, 3L, "userId", 1L);
   }
 
   @Test(expected = ResourceNotFound.class)
   public void deleteCommentForNonExistingProject() {
+    when(commentService.deleteComment(1L, 1L, "userId"))
+            .thenThrow(new ResourceNotFound(CommentService.class, PROJECT_DOES_NOT_EXIST));
     commentService.deleteComment(1L, 1L, "userId");
   }
 
   @Test(expected = ResourceNotFound.class)
   public void deleteCommentIdForExistingProjectNotFound() {
+    when(commentService.deleteComment(2L, 1L, "userId"))
+            .thenThrow(new ResourceNotFound(CommentService.class, COMMENT_NOT_FOUND, String.format(COMMENT_NOT_FOUND, 2L)));
     commentService.deleteComment(2L, 1L, "userId");
   }
 
   @Test(expected = ForbiddenException.class)
   public void deleteCommentIsEmptyOrBelongToAnotherAuthor() {
+    when(commentService.deleteComment(3L, 1L, "userId"))
+            .thenThrow(new ForbiddenException(CommentService.class, CANNOT_DELETE_COMMENT, String.format(CANNOT_DELETE_COMMENT, 3L)));
     commentService.deleteComment(3L, 1L, "userId");
   }
 
   @Test(expected = BadRequestException.class)
   public void deleteCommentInvalidCommentId() {
-    commentService.deleteComment(4L, 1L, "userId");
+    when(commentService.deleteComment(3L, 1L, "approvedCoordinatorId"))
+            .thenThrow(new BadRequestException(EmptyResultDataAccessException.class, INVALID_COMMENTID_ID, String.format("%s: %s", INVALID_COMMENTID_ID, 3L)));
+    commentService.deleteComment(3L, 1L, "approvedCoordinatorId");
   }
 
 }
