0a27b0e706e194b474084c74f8dfab0223bfd4f5
Marin <marin@bend.md>
2022-10-10 00:59:59 +0300

    [NUM-1967] - Provide easy to use loglevel-switching for all components. One more retry

--- src/test/java/de/vitagroup/num/integrationtesting/tests/NumLoggerIt.java
+++ src/test/java/de/vitagroup/num/integrationtesting/tests/NumLoggerIt.java
@@ -2,8 +2,11 @@ package de.vitagroup.num.integrationtesting.tests;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -23,9 +26,19 @@ public class NumLoggerIt extends IntegrationTest {
     @InjectMocks
     private NumLogger numLogger;
 
+    JoinPoint joinPoint;
+
     @Before
     public void setup() {
-        numLogger = org.mockito.Mockito.mock(NumLogger.class);
+        numLogger = mock(NumLogger.class);
+
+        JoinPoint joinPoint = mock(JoinPoint.class);
+        MethodSignature signature = mock(MethodSignature.class);
+        String typeName = "PUT";
+
+        when(joinPoint.getSignature()).thenReturn(signature);
+        when(joinPoint.getSignature().getDeclaringTypeName()).thenReturn(typeName);
+        when(numLogger.logMethodCall(joinPoint)).thenReturn(true);
     }
 
     @Test(expected = Exception.class)
@@ -35,17 +48,17 @@ public class NumLoggerIt extends IntegrationTest {
                 "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"));
         SecurityContextHolder.setContext(context);
         String exception = "Cannot log audit log class java.lang.String cannot be cast to class org.springframework.security.oauth2.jwt.Jwt (java.lang.String is in module java.base of loader 'bootstrap'; org.springframework.security.oauth2.jwt.Jwt is in unnamed module of loader 'app')";
-        when(numLogger.logMethodCall(null))
+        when(numLogger.logMethodCall(joinPoint))
                 .thenThrow(new Exception(exception));
-        assertTrue(numLogger.logMethodCall(null));
+        assertTrue(numLogger.logMethodCall(joinPoint));
     }
 
     @Test
     public void logMethodCallAuthentication() {
         SecurityContextHolder.getContext().setAuthentication(null);
-        when(numLogger.logMethodCall(null))
+        when(numLogger.logMethodCall(joinPoint))
                 .thenReturn(Boolean.FALSE);
-        assertFalse(numLogger.logMethodCall(null));
+        assertFalse(numLogger.logMethodCall(joinPoint));
     }
 
     @Test
@@ -54,9 +67,9 @@ public class NumLoggerIt extends IntegrationTest {
         context.setAuthentication(new BearerTokenAuthenticationToken(
                 "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"));
         SecurityContextHolder.setContext(context);
-        when(numLogger.logMethodCall(null))
+        when(numLogger.logMethodCall(joinPoint))
                 .thenReturn(Boolean.TRUE);
-        assertTrue(numLogger.logMethodCall(null));
+        assertTrue(numLogger.logMethodCall(joinPoint));
     }
 
 }
