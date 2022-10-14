c7b9a4bfc9f7e5451cbbc16992094158916cf7d2
cdeac <cristina.deac@she.net>
2022-10-14 17:06:42 +0300

    update JUnit tests for sonarcloud coverage

--- src/test/java/de/vitagroup/num/service/ehrbase/ParameterServiceTest.java
+++ src/test/java/de/vitagroup/num/service/ehrbase/ParameterServiceTest.java
@@ -39,6 +39,7 @@ public class ParameterServiceTest {
 
     private static final String ARCHETYPE_ID_VALUE_SYMBOL_VALUE = "openEHR-EHR-CLUSTER.laboratory_test_analyte.v1";
     private static final String ARCHETYPE_ID_VALUE = "openEHR-EHR-OBSERVATION.blood_pressure.v2";
+    private static final String ARCHETYPE_ID_VALUE_UNITS = "openEHR-EHR-OBSERVATION.blood_pressure.v2";
 
     ///items[at0002]/value/defining_code/code_string
     // /data[at0002]/items[at0022]/value/defining_code/code_string
@@ -49,6 +50,8 @@ public class ParameterServiceTest {
     private static final String AQL_PATH_VALUE_SYMBOL_VALUE = "/items[at0009]/value/symbol/value";
 
     private static final String AQL_PATH_VALUE = "/data[at0001]/events[at0006]/time/value";
+
+    private static final String AQL_PATH_UNITS = "/data[at0001]/events[at0006]/value/units";
 
     private static final String AQL_CODE_QUERY = "Select distinct e0/data[at0002]/items[at0019] as F1 from EHR e contains EVALUATION e0[openEHR-EHR-EVALUATION.gender.v1] order by e0/data[at0002]/items[at0019] ASCENDING";
 
@@ -70,17 +73,30 @@ public class ParameterServiceTest {
     @MethodSource("testInput")
     public void getParameterValuesTest() {
 
-//        QueryResponseData response = new QueryResponseData();
-//
-//        response.setColumns(new ArrayList<>(List.of(Map.of("path", "/ehr_id/value"), Map.of("uuid", "c/uuid"))));
-//        response.setRows(  List.of(
-//                new ArrayList<>(List.of("testehrid1", Map.of("_type", "OBSERVATION", "uuid", "12345"))),
-//                new ArrayList<>(List.of("testehrid2", Map.of("_type", "SECTION", "uuid", "bla")))));
+        LinkedHashMap e1= new LinkedHashMap();
+        e1.put("name", Map.of("value", "Geschlecht bei der Geburt", "_type", "DV_TEXT"));
+        e1.put("value",Map.of("_type", "DV_CODED_TEXT",
+                "value", "Male",
+                "defining_code", Map.of("_type", "CODE_PHRASE",
+                                            "code_string", "male",
+                                            "terminology_id", Map.of("_type", "TERMINOLOGY_ID",
+                                                                        "value","http://hl7.org/fhir/administrative-gender"))));
+        e1.put("_type", "ELEMENT");
+        e1.put("archetype_node_id", "at0019");
+        LinkedHashMap e2= new LinkedHashMap();
+        e2.put("name", Map.of("value", "Geschlecht bei der Geburt", "_type", "DV_TEXT"));
+        e2.put("value",Map.of("_type", "DV_CODED_TEXT",
+                              "value", "divers",
+                              "defining_code", Map.of("_type", "CODE_PHRASE", "code_string", "D",
+                                                          "terminology_id", Map.of("_type", "TERMINOLOGY_ID",
+                                                                                      "value","http://hl7.org/fhir/administrative-gender"))));
+        e2.put("_type", "ELEMENT");
+        e2.put("archetype_node_id", "at0019");
         QueryResponseData queryResponseData = new QueryResponseData();
         queryResponseData.setColumns(new ArrayList<>(List.of(Map.of("path", "/ehr_id/value"), Map.of("uuid", "c/uuid"))));
-        queryResponseData.setRows(  List.of(
-                new ArrayList<>(List.of("name", Map.of("value", "Geschlecht bei der Geburt", "_type", "DV_TEXT"))),
-                new ArrayList<>(List.of("value", Map.of("_type", "DV_CODED_TEXT", "value", "Male")))));
+        queryResponseData.setRows( List.of(
+                new ArrayList<>(List.of(e1)),
+                new ArrayList<>(List.of(e2))));
         Mockito.when(ehrBaseService.executePlainQuery(Mockito.anyString())).thenReturn(queryResponseData);
         parameterService.getParameterValues("approvedUserId", aqlPath, arhcetypeId);
     }
@@ -91,7 +107,8 @@ public class ParameterServiceTest {
                 {AQL_PATH_VALUE_MAGNITUDE, ARCHETYPE_ID_VALUE_MAGNITUDE},
                 {AQL_PATH_VALUE_VALUE, ARCHETYPE_ID_VALUE_VALUE},
                 {AQL_PATH_VALUE, ARCHETYPE_ID_VALUE},
-                {AQL_PATH_VALUE_SYMBOL_VALUE, ARCHETYPE_ID_VALUE_SYMBOL_VALUE}
+                {AQL_PATH_VALUE_SYMBOL_VALUE, ARCHETYPE_ID_VALUE_SYMBOL_VALUE},
+                {AQL_PATH_UNITS, ARCHETYPE_ID_VALUE}
         });
     }
 }
