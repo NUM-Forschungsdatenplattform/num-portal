ba6fa8dec471048d6eea037727122b2dc2dada29
cdeac <cristina.deac@she.net>
2022-11-04 08:34:35 +0200

    more tests

--- src/test/java/de/vitagroup/num/domain/specification/ProjectSpecificationTest.java
+++ src/test/java/de/vitagroup/num/domain/specification/ProjectSpecificationTest.java
@@ -1,6 +1,7 @@
 package de.vitagroup.num.domain.specification;
 
 import de.vitagroup.num.domain.Project;
+import de.vitagroup.num.domain.ProjectStatus;
 import de.vitagroup.num.domain.Roles;
 import de.vitagroup.num.domain.dto.SearchFilter;
 import org.junit.Test;
@@ -70,6 +71,7 @@ public class ProjectSpecificationTest {
         Set<String> usersUUID = new HashSet<>();
         usersUUID.add("user-id-1");
         usersUUID.add("user-id-2");
+        Mockito.when(coordinator.get("userId").in(usersUUID)).thenReturn(Mockito.mock(Predicate.class));
         ProjectSpecification ps = ProjectSpecification.builder()
                 .filter(filter)
                 .roles(Arrays.asList(Roles.RESEARCHER))
@@ -119,5 +121,25 @@ public class ProjectSpecificationTest {
                 .build();
         ps.toPredicate(root, criteriaBuilder);
         Mockito.verify(root, Mockito.times(2)).get("status");
+    }
+
+    @Test
+    public void getAllExceptArchivedSpecificationTest() {
+        Join coordinator = Mockito.mock(Join.class);
+        Mockito.when(root.join("coordinator", JoinType.INNER)).thenReturn(coordinator);
+        Path statusPath = Mockito.mock(Path.class);
+        Mockito.when(root.get("status")).thenReturn(statusPath);
+        Map<String, String> filter = new HashMap<>();
+        filter.put("type", SearchFilter.ALL.name());
+        ProjectSpecification ps = ProjectSpecification
+                .builder()
+                .roles(Arrays.asList(Roles.STUDY_APPROVER))
+                .loggedInUserId("userId")
+                .loggedInUserOrganizationId(5L)
+                .filter(filter)
+                .build();
+        ps.toPredicate(root, criteriaBuilder);
+        Mockito.verify(root, Mockito.times(2)).get("status");
+        Mockito.verify(criteriaBuilder, Mockito.times(1)).notEqual(Mockito.eq(statusPath), Mockito.eq(ProjectStatus.ARCHIVED));
     }
 }
