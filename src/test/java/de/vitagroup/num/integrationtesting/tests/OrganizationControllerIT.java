2ecc3c4e764345b84b788ad96b668d3386bbf31f
cdeac <cristina.deac@she.net>
2023-07-24 14:21:29 +0300

    update organization integration test

--- src/test/java/de/vitagroup/num/integrationtesting/tests/OrganizationControllerIT.java
+++ src/test/java/de/vitagroup/num/integrationtesting/tests/OrganizationControllerIT.java
@@ -1,13 +1,25 @@
 package de.vitagroup.num.integrationtesting.tests;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.vitagroup.num.domain.Organization;
+import de.vitagroup.num.domain.admin.UserDetails;
+import de.vitagroup.num.domain.dto.OrganizationDto;
+import de.vitagroup.num.domain.repository.OrganizationRepository;
+import de.vitagroup.num.domain.repository.UserDetailsRepository;
 import de.vitagroup.num.integrationtesting.security.WithMockNumUser;
 import lombok.SneakyThrows;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.Collections;
+import java.util.Optional;
 
 import static de.vitagroup.num.domain.Roles.SUPER_ADMIN;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -19,6 +31,15 @@ public class OrganizationControllerIT extends IntegrationTest {
   public static final Long INVALID_ORGANIZATION_ID = 12L;
 
   private static final String ORGANIZATION_PATH = "/organization";
+
+  @Autowired
+  private ObjectMapper mapper;
+
+  @Autowired
+  private OrganizationRepository organizationRepository;
+
+  @Autowired
+  private UserDetailsRepository userDetailsRepository;
 
   @Test
   @SneakyThrows
@@ -64,5 +85,68 @@ public class OrganizationControllerIT extends IntegrationTest {
                     .queryParam("page", "0")
                     .queryParam("filter[search]", "dummySearchInput"))
             .andExpect(status().isOk());
+  }
+
+  @Test
+  @SneakyThrows
+  @WithMockNumUser(roles = {SUPER_ADMIN})
+  public void shouldSaveAndDeleteOrganizationSuccessfully() {
+    OrganizationDto request = OrganizationDto.builder()
+            .name("organization Test")
+            .mailDomains(Collections.emptySet())
+            .build();
+    String organizationJson = mapper.writeValueAsString(request);
+
+    MvcResult result =
+            mockMvc
+                    .perform(
+                            post(ORGANIZATION_PATH)
+                                    .with(csrf())
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content(organizationJson))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.name").value(request.getName()))
+                    .andReturn();
+
+    OrganizationDto dto = mapper.readValue(result.getResponse().getContentAsString(), OrganizationDto.class);
+
+    mockMvc
+            .perform(
+                    delete(String.format("%s/%s", ORGANIZATION_PATH, dto.getId()))
+                            .with(csrf())
+                            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
+  }
+
+  @Test
+  @SneakyThrows
+  @WithMockNumUser(roles = {SUPER_ADMIN})
+  public void shouldHandleDeleteOrganizationWithUsers() {
+    OrganizationDto request = OrganizationDto.builder()
+            .name("Organization TestDelete")
+            .mailDomains(Collections.emptySet())
+            .build();
+    String organizationJson = mapper.writeValueAsString(request);
+
+    mockMvc
+            .perform(
+                    post(ORGANIZATION_PATH)
+                            .with(csrf())
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(organizationJson))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.name").value(request.getName()))
+            .andReturn();
+
+    Optional<Organization> organization = organizationRepository.findByName("Organization TestDelete");
+    UserDetails userDetails = UserDetails.builder().userId("userOne").approved(true).organization(organization.get()).build();
+    userDetailsRepository.save(userDetails);
+
+    mockMvc
+            .perform(
+                    delete(String.format("%s/%s", ORGANIZATION_PATH, organization.get().getId()))
+                            .with(csrf())
+                            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isBadRequest());
   }
 }
