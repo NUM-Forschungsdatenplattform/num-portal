979466cc6b8801fbbcfe7e283faccdb65e3c1216
cazacmarin <cazacmarin@yahoo.fr>
2023-08-04 16:56:58 +0300

    [NUM-2229] - BE filter organizations by 'active' field (#404)
    * [NUM-2229] - BE filter organizations by 'active' field
    
    * [NUM-2229] - BE filter organizations by 'active' field. Adding filtering. Until now was only sorting
    
    * [NUM-2229] - BE filter organizations by 'active' field. Adding some tests
    
    * [NUM-2229] - BE filter organizations by 'active' field. Removing Sort by Cristina Request.
    
    ---------
    
    Co-authored-by: Marin <marin@bend.md>

--- src/main/java/de/vitagroup/num/service/OrganizationService.java
+++ src/main/java/de/vitagroup/num/service/OrganizationService.java
@@ -17,6 +17,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.*;
