26fc6ecf6d47cd72ab137b17008da041782e04e0
cdeac <cristina.deac@she.net>
2023-08-07 16:15:18 +0300

    cleanup

--- src/test/java/de/vitagroup/num/service/logger/NumLoggerTest.java
+++ src/test/java/de/vitagroup/num/service/logger/NumLoggerTest.java
@@ -73,7 +73,5 @@ public class NumLoggerTest {
     private Method testMethod() throws NoSuchMethodException {
         return getClass().getDeclaredMethod("someTestMethod");
     }
-    private void someTestMethod() {
-    }
 
 }
