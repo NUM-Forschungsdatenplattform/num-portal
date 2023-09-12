d44980cb6d1cab48996876b3f10753dba0df208d
cdeac <cristina.deac@she.net>
2023-09-12 10:52:17 +0300

    cleanup

--- src/main/java/de/vitagroup/num/web/feign/KeycloakFeign.java
+++ src/main/java/de/vitagroup/num/web/feign/KeycloakFeign.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "keycloak", url = "${userstore.url}")
 public interface KeycloakFeign {
