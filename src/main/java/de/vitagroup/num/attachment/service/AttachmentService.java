9bb4ff280c1ebc7ed4c38f5e1660f673e054aa49
Marin <marin@bend.md>
2023-10-06 09:51:20 +0300

    [NUM-2280] - Allow saving a document.

--- src/main/java/de/vitagroup/num/attachment/service/AttachmentService.java
+++ src/main/java/de/vitagroup/num/attachment/service/AttachmentService.java
@@ -6,8 +6,10 @@ import de.vitagroup.num.attachment.domain.model.Attachment;
 import de.vitagroup.num.domain.templates.ExceptionsTemplate;
 import de.vitagroup.num.service.exception.BadRequestException;
 import de.vitagroup.num.service.exception.ResourceNotFound;
-import lombok.AllArgsConstructor;
+import de.vitagroup.num.web.controller.NumAttachmentController;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,16 +17,24 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+
+import static de.vitagroup.num.domain.templates.ExceptionsTemplate.DOCUMENT_TYPE_MISMATCH;
+import static de.vitagroup.num.domain.templates.ExceptionsTemplate.INVALID_FILE_MISSING_CONTENT;
+import static de.vitagroup.num.domain.templates.ExceptionsTemplate.PDF_FILE_SIZE_EXCEEDED;
 
 @Service
 @Transactional("attachmentTransactionManager")
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Slf4j
 @ConditionalOnProperty(prefix = "num", name = "enableAttachmentDatabase", havingValue = "true")
 public class AttachmentService {
 
     private final AttachmentRepository attachmentRepository;
+
+    @Value("${num.pdfFileSize:10485760}")
+    private long pdfFileSize;
 
     public List<Attachment> listAttachments() {
         return attachmentRepository.getAttachments();
@@ -38,9 +48,7 @@ public class AttachmentService {
     }
 
     public void saveAttachment(MultipartFile file, String description, String loggedInUserId) throws IOException {
-        if (file.isEmpty()) {
-            throw new BadRequestException(AttachmentService.class, "Invalid file. Missing content");
-        }
+        validate(file);
         AttachmentDto model = AttachmentDto.builder()
                 .name(file.getOriginalFilename())
                 .description(description)
@@ -49,6 +57,19 @@ public class AttachmentService {
                 .content(file.getBytes())
                 .build();
         attachmentRepository.saveAttachment(model);
+    }
+
+    private void validate(MultipartFile file) {
+        if (file.isEmpty()) {
+            throw new BadRequestException(AttachmentService.class, INVALID_FILE_MISSING_CONTENT);
+        }
+        if (!Objects.requireNonNull(file.getOriginalFilename()).toLowerCase().endsWith(".pdf")){
+            throw new BadRequestException(NumAttachmentController.class, DOCUMENT_TYPE_MISMATCH);
+        }
+        if (file.getSize() > pdfFileSize){
+            throw new BadRequestException(NumAttachmentController.class,
+                    String.format(PDF_FILE_SIZE_EXCEEDED, pdfFileSize/1048576, file.getSize()/1048576));
+        }
     }
 
     public void deleteById(Long id, String loggedInUserId) {
