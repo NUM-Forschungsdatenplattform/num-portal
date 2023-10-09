ad76cf47a3d01df56ac5ca9dd81a32cb7f09ca09
cazacmarin <cazacmarin@yahoo.fr>
2023-10-09 12:35:04 +0300

    Feature/num 2280 allow saving a document (#428)
    * [NUM-2276] - Health announcements not appearing
    
    * [NUM-2280] - Allow saving a document.
    
    * [NUM-2280] - Allow saving a document. Fixing test
    
    * [NUM-2280] - Allow saving a document. Adding more tests
    
    * [NUM-2280] - Allow saving a document. Adding additional pdf check
    
    * [NUM-2280] - Allow saving a document. Adding additional pdf check
    
    * [NUM-2280] - Allow saving a document. Fixing one test
    
    ---------
    
    Co-authored-by: Marin <marin@bend.md>

--- src/test/java/de/vitagroup/num/attachment/service/AttachmentServiceTest.java
+++ src/test/java/de/vitagroup/num/attachment/service/AttachmentServiceTest.java
@@ -3,7 +3,9 @@ package de.vitagroup.num.attachment.service;
 import de.vitagroup.num.attachment.AttachmentRepository;
 import de.vitagroup.num.attachment.domain.dto.AttachmentDto;
 import de.vitagroup.num.attachment.domain.model.Attachment;
+import de.vitagroup.num.service.exception.BadRequestException;
 import de.vitagroup.num.service.exception.ResourceNotFound;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,10 +14,15 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.Optional;
+
+import static de.vitagroup.num.domain.templates.ExceptionsTemplate.DOCUMENT_TYPE_MISMATCH;
+import static de.vitagroup.num.domain.templates.ExceptionsTemplate.INVALID_FILE_MISSING_CONTENT;
+import static de.vitagroup.num.domain.templates.ExceptionsTemplate.PDF_FILE_SIZE_EXCEEDED;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AttachmentServiceTest {
@@ -67,8 +74,42 @@ public class AttachmentServiceTest {
 
     @Test
     public void saveAttachmentTest() throws IOException {
-        MultipartFile mockFile = new MockMultipartFile("testFile", "testFile.pdf", "application/pdf", "content".getBytes());
+        ReflectionTestUtils.setField(attachmentService, "pdfFileSize", 10485760);
+        MultipartFile mockFile = new MockMultipartFile("testFile", "testFile.pdf", "application/pdf", "%PDF-1.5content".getBytes());
         attachmentService.saveAttachment(mockFile, null, "author-id");
         Mockito.verify(attachmentRepository, Mockito.times(1)).saveAttachment(Mockito.any(AttachmentDto.class));
+    }
+
+    @Test
+    public void checkEmptyFileError() throws IOException {
+        ReflectionTestUtils.setField(attachmentService, "pdfFileSize", 10485760);
+        MultipartFile mockFile = new MockMultipartFile("testFile", "testFile.pdf", "application/pdf", "".getBytes());
+        try {
+            attachmentService.saveAttachment(mockFile, null, "author-id");
+        }catch (BadRequestException fe) {
+            Assert.assertEquals(INVALID_FILE_MISSING_CONTENT, fe.getMessage());
+        }
+    }
+
+    @Test
+    public void checkDocumentTypeMismatchException() throws IOException {
+        ReflectionTestUtils.setField(attachmentService, "pdfFileSize", 10485760);
+        MultipartFile mockFile = new MockMultipartFile("testFile", "testFile", "application/pdf", "%PDF-1.5 content".getBytes());
+        try {
+            attachmentService.saveAttachment(mockFile, null, "author-id");
+        }catch (BadRequestException fe) {
+            Assert.assertEquals(DOCUMENT_TYPE_MISMATCH, fe.getMessage());
+        }
+    }
+
+    @Test
+    public void pdfFileExceededException() throws IOException {
+        ReflectionTestUtils.setField(attachmentService, "pdfFileSize", 1);
+        MultipartFile mockFile = new MockMultipartFile("testFile", "testFile.pdf", "application/pdf", "%PDF-1.5".getBytes());
+        try {
+            attachmentService.saveAttachment(mockFile, null, "author-id");
+        }catch (BadRequestException fe) {
+            Assert.assertEquals(String.format(PDF_FILE_SIZE_EXCEEDED, 0, 0), fe.getMessage());
+        }
     }
 }
