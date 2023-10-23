bd278fb3780f9719cd6d4f9eca259607388ef15a
cdeac <cristina.deac@she.net>
2023-10-23 13:13:30 +0300

    NUM-2299 fix binding to request body

--- src/main/java/de/vitagroup/num/domain/dto/SlimAqlDto.java
+++ src/main/java/de/vitagroup/num/domain/dto/SlimAqlDto.java
@@ -3,11 +3,15 @@ package de.vitagroup.num.domain.dto;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SlimAqlDto {
 
   @NotNull(message = "Query cannot be null")
