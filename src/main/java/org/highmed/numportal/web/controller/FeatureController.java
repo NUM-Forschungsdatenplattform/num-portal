78ff354636776d2a7d7220084a9a0e4b260f965e
rabeaM <rabea.mueller@sva.de>
2024-11-13 15:40:30 +0100

    checkstyle rules

--- src/main/java/org/highmed/numportal/web/controller/FeatureController.java
+++ src/main/java/org/highmed/numportal/web/controller/FeatureController.java
@@ -1,9 +1,10 @@
 package org.highmed.numportal.web.controller;
 
+import org.highmed.numportal.properties.FeatureProperties;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.AllArgsConstructor;
-import org.highmed.numportal.properties.FeatureProperties;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/feature", produces = "application/json")
 @SecurityRequirement(name = "security_auth")
 public class FeatureController {
+  FeatureProperties featureProperties;
 
-    FeatureProperties featureProperties;
-
-    @GetMapping
-    @Operation(description = "Get feature flags")
-    public ResponseEntity<FeatureProperties> getFeatureFlags() {
-        return ResponseEntity.ok(featureProperties);
-    }
+  @GetMapping
+  @Operation(description = "Get feature flags")
+  public ResponseEntity<FeatureProperties> getFeatureFlags() {
+    return ResponseEntity.ok(featureProperties);
+  }
 }
